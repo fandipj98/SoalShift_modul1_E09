0637b9c7f67a9617b50da050e902ffa827bc18aa
Fadhil Musaad <fadhilmusaad@gmail.com>
2019-02-21 19:27:16 +0700

    update soal4decrypt

--- soal4decrypt.sh
+++ soal4decrypt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sh=`date +"%H"`
+read NOW
+sh=${NOW:0:2}
 source1=( {a..z} )
 trans1=()
 trans1+=( ${source1[@]:(-(26-$sh))} )
@@ -11,6 +12,5 @@ trans2+=( ${source2[@]:(-(26-$sh))} )
 trans2+=( ${source2[@]:0:$(($sh))} )
 source1+=( ${source2[@]} )
 trans1+=( ${trans2[@]} )
-read NOW
 < "$NOW" > "$NOW Decrypt" tr "${trans1[*]}" "${source1[*]}"
 
